fefd3fd107d7570efaa0030ee40fbc5a5ea66867
zhouy <773858927@qq.com>
2017-04-05 13:23:58 +0800

    _setattr_ _getattr_ 两者使用

---
+++ 3.py
@@ -0,0 +1,24 @@
+class Rectangle:
+    def __init__(self):
+        self.width = 0
+        self.hight = 0
+
+    def __setattr__(self, name, value):
+        if name == 'size':
+            self.hight,self.width = value
+        else:
+            self.__dict__[name] = value
+    def __getattr__(self, name):
+        if name == 'size':
+            return self.hight,self.width
+        else:
+            AttributeError
+
+if __name__ == '__main__':
+    rt = Rectangle()
+    rt.hight = 10
+    rt.width = 5
+    #print(rt.__setattr__('size', (11, 6)))
+    print(rt.__getattr__('size'))
+
+

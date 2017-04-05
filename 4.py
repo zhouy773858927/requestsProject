30107aa8622f7251efc8fa32cd52acd19113baaa
zhouy <773858927@qq.com>
2017-04-05 14:09:21 +0800

    斐波那契数列迭代器 与从可迭代对象中获得数值完善 与从迭代器中得到序列

--- 4.py
+++ 4.py
@@ -8,14 +8,27 @@ class Fibs:
     def __iter__(self):
         return self
 
+class TestIterator:
+    vaule = 0
+    def __next__(self):
+        self.vaule += 1
+        if self.vaule > 10: raise StopIteration
+        return self.vaule
+    def __iter__(self):
+        return self
+
 if __name__ == '__main__':
     fibs = Fibs()
     for f in fibs:
         if f > 1000:
             print(f)
             break
+    ti = TestIterator()
 
-print("-------------------------------------------分割线------------------------------------------")
+print("-------------------------------------------第一次分割线------------------------------------------")
 it = iter([1,2,3,4,5])
 print(it.__next__())
 print(it.__next__())
+print("-------------------------------------------第二次分割线------------------------------------------")
+print(list(ti))
+
